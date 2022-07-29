7c26010a41e58b75ff3c500bf469eef288f77d31
eduardocsanchez <chiquito.7v@gmail.com>
2022-07-29 18:37:06 -0500

    updated the unit tests

--- test.js
+++ test.js
@@ -46,6 +46,43 @@ const salesOrderss = [{
 describe("Is an array Objects", function () {
   it("test", function () {
     expect(array).toContain(jasmine.objectContaining(salesOrderss));
+    //expect(array).toEqual(salesOrderss.arrayContaining(["id", "created", "quantity"]));
   });
 });
 
+
+describe("jasmine.objectContaining", function () {
+  var obj;
+
+  beforeEach(function () {
+    obj = {
+      id: 'S1',
+
+      created: '2020-01-02',
+
+      quantity: 6
+    };
+  });
+
+  it("matches objects with the expect key/value pairs", function () {
+    expect(obj).toEqual(jasmine.objectContaining({
+      id: "S1",
+      created: "2020-01-02",
+      quantity: 6
+    }));
+ 
+  });
+});
+
+var obj = [];
+describe("is an empy object", function () {
+  it("test for empty object", function () {
+    expect(obj.length).toBe(0);
+  });
+});
+
+describe("is not an empy object", function () {
+  it("test for not empty object", function () {
+    expect(salesOrderss.length).toBeGreaterThan(0);
+  });
+});
